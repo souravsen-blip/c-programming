36acf50630b374b4670be886d68e4294a6734cfd
souravsen-blip <souravsenchandanngar@gmail.com>
2026-09-25 20:43:26 +0530

    even using while loop

--- Untitled1.c
+++ Untitled1.c
@@ -1,12 +1,12 @@
 // WRITE A C PROGRAME TO PRINT EVEN NUMBER UPTO 10
 #include<stdio.h>
 int main(){
-	int i=2;
-	while (i<=10)
-	{
+	int i=2,n;
+	printf("enter n:");
+	scanf("%d",&n);
+	while(i<=n){
 		printf("%d\n",i);
 		i=i+2;
 	}
 	return 0;
 }
-
